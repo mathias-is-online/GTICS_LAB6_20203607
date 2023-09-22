1c3bfedf1d58509bcf6077cf042afbbdc791ea3e
Mortem <a20203607@pucp.edu.pe>
2023-09-21 20:34:05 -0500

    ola

---
+++ LAB6_20203607/src/test/java/com/example/lab6_20203607/Lab620203607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20203607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab620203607ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
